a248f2b52656e3cd7df3f8f1a6cb31643c188f30
pedrolimagit <pedrolimadevmail@gmail.com>
2026-01-22 16:00:26 -0300

    feat(controller): Foi criado uma nova controller e uma models para usuarios, e tambem uma nova interface IEnumerable

---
+++ Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using ApiCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCrud.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<User>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public async Task<ActionResult<IEnumerable<User>>> GetAll()
+        {
+            try
+            {
+                return Ok(null);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                  new { message = "Erro ao buscar usuários", error = ex.Message });
+
+            }
+
+        }
+    }
+}
