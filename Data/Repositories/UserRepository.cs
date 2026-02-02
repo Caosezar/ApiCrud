5c21a172f5486f3873d47c6709dc2705f042ac62
pedrolimagit <pedrolimadevmail@gmail.com>
2026-02-02 14:29:53 -0300

    feat(services/repository): implementação dos métodos CreateUserAsync AddUserAsync

--- Data/Repositories/UserRepository.cs
+++ Data/Repositories/UserRepository.cs
@@ -19,5 +19,11 @@ namespace ApiCrud.Data.Repositories
         {
             return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
         }
+        public async Task<User?> AddUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
     }
 }
